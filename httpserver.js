6b22511be5d841e9a01a5b4da30da09eabe12cd2
deepakporwal <er.deepakporwal@gmail.com>
2022-05-03 19:40:16 +0530

    server static file using http server

--- httpserver.js
+++ httpserver.js
@@ -1,11 +1,27 @@
 const http = require('http');
+const fs = require('fs');
 
 const port = process.env.port ||3004;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 201;
-    res.setHeader('content-Type', 'text/html');
-    res.end('<h1> This is response from sample server</h1>');
+    if(req.url =='/'){
+        res.statusCode = 201;
+        res.setHeader('content-Type', 'text/html');
+        res.end('<h1> This is response from sample server</h1>');
+    }
+
+    else if(req.url =='/hello'){
+        res.statusCode = 201;
+        const data = fs.readFileSync('index.html');
+        res.end(data.toString());
+    }
+    else
+    {
+        res.statusCode = 404;
+        res.setHeader('content-Type', 'text/html');
+        res.end('<h1> Content not found</h1>');
+    }
+   
 })
 
 server.listen(port, () => {
